b14fe992ec61becf765a7fa685804187aab5f70b
quasean65 <qshelton114@boyslatin.net>
2026-09-03 15:05:06 +0000

    class work

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+# STRING - characters that are wrapped within quotes.
+# characters can be letters, numbers, and symbols.
+# you can use both single and double quotation
+# marks for strings.
+
+"Qua'sean Shelton"
+'123 main street'
+'this cost $100 dollars'
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer
+
+
+
+
+
+
+
+
+.
