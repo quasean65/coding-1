82068427f539a3e2b53a0edcca43b00b9ef8bc59
quasean65 <qshelton114@boyslatin.net>
2026-09-03 15:05:56 +0000

    class work

---
+++ variableActitivty.py
@@ -0,0 +1,12 @@
+apple = 1.00
+orange 1.05
+
+
+#1. A variable MUST start with letter and cannot start with a number 
+#2. A varaible can be a mixed of letters and numbers BUT not symbols
+#3. AA variable CANNOT be a Phython keyword 
+#4. Variables with multiple words do not have spaces
+
+applesAtStore = 3
+kidsDoingMath = 20
+noInsuranceOnTrip 
