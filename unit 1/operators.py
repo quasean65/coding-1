2dffcd058ef3cb21d5ffecdc5b7ad8df0d2fc877
quasean65 <qshelton114@boyslatin.net>
2026-09-09 15:15:20 +0000

    class work

---
+++ unit 1/operators.py
@@ -0,0 +1,16 @@
+# Assigment Operators- Exclusively used to
+# assign values to variables. 
+# think key / value pairings 
+
+# We use a sinmgle equal sign to represent the assingment 
+# operator
+name = Qua'sean 
+grade = 10
+school = Boys latin
+
+# Arithmetic Operqtors- Used on numerical
+# data types to perform calculations.
+# intergers ( whole numbers) an floats (decimal numbers)
+
+# print is a function that lets us show code 
+# in the terminal
