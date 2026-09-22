04a9a9bd86c37f217cb4a9caf94d12945798c6cf
quasean65 <qshelton114@boyslatin.net>
2026-09-22 15:08:36 +0000

    class work

---
+++ unit 1/builtinFunctions.py
@@ -0,0 +1,22 @@
+# Data casting functions
+# these are built in functions (pre-written intructions)
+# that change data types from one form into another
+
+# str () - this datacasting function allows you to change
+# any data type into a string
+
+year = 1906
+print( "This event took place in "+ str(year))
+
+# string concatenation - combining string data types
+# with one another. We use the plus sign (+) operator
+# to combine strings together
+
+print(3+3) # ADDING 2 interger
+
+# int()- Any data type passed into the brackets will
+# # be converted into a integer(whole number)
+
+num1 = input("type in a number: ")
+# input always returns a string
+print(4 + num1)
