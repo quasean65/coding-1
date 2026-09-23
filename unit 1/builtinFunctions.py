dd7a9f457056b7d7d0896bb448d664dbd0707139
quasean65 <qshelton114@boyslatin.net>
2026-09-23 15:26:33 +0000

    class work

--- unit 1/builtinFunctions.py
+++ unit 1/builtinFunctions.py
@@ -20,3 +20,10 @@ print(3+3) # ADDING 2 interger
 num1 = input("type in a number: ")
 # input always returns a string
 print(4 + num1)
+
+# float() - A function that will change any datatype
+# passed into it it, into float/ decimal number
+
+num2 = input("type in a number")
+# input always returns a string
+print(9 + float num2))
---
+++ unit 1/functionActivity1.py
@@ -0,0 +1,12 @@
+# we are comparing the number 85 to a number we type in 
+# using input ()
+# if the number is greater than 85 than we primt true
+# if it is lower we print false
+
+num1 = input("type in a number: ")
+print(int(num) > 85)
+
+
+# 2.
+num2= input(" hey im here type something!!")
+print(str(360)+ num2)
